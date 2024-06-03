54517b1a5b2238eb76ea670b8a2de7afd089825a
panserbjorn <joaco.silveira@gmail.com>
2024-06-03 09:18:56 +0200

    Routers use the crud module to consume from database

--- InsuranceAPI/routers/auth_router.py
+++ InsuranceAPI/routers/auth_router.py
@@ -38,17 +38,17 @@ def login(
 
 @router.get("/users", tags=["users"], description="Get all users")
 def get_users(db: Session = Depends(get_db)):
-    return data.get_users()
+    return crud.get_users(db)
 
 
 @router.get("/users/{user_id}", tags=["users"], description="Get a user by ID")
 def get_user_by_id(user_id: str, db: Session = Depends(get_db)):
-    return data.get_user_data(user_id)
+    return crud.get_user_by_id(db, user_id)
 
 
 @router.get("/users/name/{user_name}", tags=["users"], description="Get a user by name")
 def get_user_by_name(user_name: str, db: Session = Depends(get_db)):
-    return data.get_user_data_by_name(user_name)
+    return crud.get_user_by_name(db, user_name)
 
 
 @router.get(
@@ -57,4 +57,4 @@ def get_user_by_name(user_name: str, db: Session = Depends(get_db)):
     description="Get all policies for a user",
 )
 def get_user_policies(user_name: str, db: Session = Depends(get_db)):
-    return data.get_user_policies(data.get_user_data_by_name(user_name)["id"])
+    return crud.get_user_policies(db, user_name)
--- InsuranceAPI/routers/policies_router.py
+++ InsuranceAPI/routers/policies_router.py
@@ -5,6 +5,8 @@ from sqlalchemy.orm import Session
 # Temporal will replace for database
 from internal.providers import DataProvider
 
+from internal import crud
+
 from database import get_db
 
 data = DataProvider()
@@ -14,14 +16,14 @@ router = APIRouter()
 
 @router.get("/policies", tags=["policies"], description="Get all policies")
 def get_policies(db: Session = Depends(get_db)):
-    return data.get_policies()
+    return crud.get_policies(db)
 
 
 @router.get(
     "/policies/{policy_id}", tags=["policies"], description="Get a policy by ID"
 )
 def get_policy_by_id(policy_id: str, db: Session = Depends(get_db)):
-    return data.get_policy_data(policy_id)
+    return crud.get_policy_by_id(db, policy_id)
 
 
 @router.get(
@@ -30,4 +32,4 @@ def get_policy_by_id(policy_id: str, db: Session = Depends(get_db)):
     description="Get the users for a policy",
 )
 def get_policy_users(policy_id: str, db: Session = Depends(get_db)):
-    return data.get_policy_users(policy_id)
+    return crud.get_policy_users(db, policy_id)
